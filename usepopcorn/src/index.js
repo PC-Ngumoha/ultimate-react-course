4674574d528c368c5580816549efb2ba61b93cd1
PC-Ngumoha <pcngumoha@gmail.com>
2026-01-21 17:06:00 +0100

    Learn: Making components flexible with props.

--- usepopcorn/src/index.js
+++ usepopcorn/src/index.js
@@ -1,14 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 // import './index.css';
 // import App from './App';
 
 import StarRating from './StarRating';
 
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+
+  return (
+    <div>
+      <StarRating
+        maxRating={10}
+        color="blue"
+        size={30}
+        onSetRating={setMovieRating}
+      />
+      <p>This movie was rated {movieRating} stars.</p>
+    </div>
+  );
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={5} />
+    <StarRating
+      maxRating={5}
+      messages={['Terrible', 'Bad', 'Okay', 'Good', 'Amazing']}
+    />
+    <StarRating
+      maxRating={10}
+      color="red"
+      size={24}
+      className="test"
+      defaultRating={3}
+    />
+
+    <Test />
   </React.StrictMode>
 );
