b64d77aab5221403ff8260d68e15c55b7bfb4680
PC-Ngumoha <pcngumoha@gmail.com>
2026-02-18 00:56:16 +0100

    Implement map view using react-leaflet.

--- worldwise/src/components/Map.jsx
+++ worldwise/src/components/Map.jsx
@@ -1,9 +1,13 @@
+import { useState } from 'react';
 import { useNavigate, useSearchParams } from 'react-router-dom';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+
 import styles from './Map.module.css';
 
 function Map() {
   const navigate = useNavigate();
   const [searchParams, setSearchParams] = useSearchParams();
+  const [mapPos, setMapPos] = useState([40, 0]);
 
   const lat = searchParams.get('lat');
   const lng = searchParams.get('lng');
@@ -11,16 +15,26 @@ function Map() {
   return (
     <div
       className={styles.mapContainer}
-      onClick={() => {
-        navigate('form');
-      }}
+      // onClick={() => {
+      //   navigate('form');
+      // }}
     >
-      <h1>
-        Position: {lat}, {lng}
-      </h1>
-      <button onClick={() => setSearchParams({ lat: 20, lng: 50 })}>
-        Change Pos
-      </button>
+      <MapContainer
+        center={mapPos}
+        zoom={13}
+        scrollWheelZoom={true}
+        className={styles.map}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+        />
+        <Marker position={mapPos}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
     </div>
   );
 }
